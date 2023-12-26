ddb4f8648e4b67c4fe83972bbc23fd39caa470e8
melvinsz <113125220+melvinsz@users.noreply.github.com>
2023-12-26 17:12:26 +0000

    Deploying to gh-pages from @ melvinsz/goit-js-hw-10@6e2254dbe5a4f39c8962d2613e504ef5e76412f9 🚀

--- catFind.8855435b.js
+++
@@ -1,2 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users").then((o=>{if(!o.ok)throw new Error(o.status);return console.log("response",o),o.json()})).then((o=>{console.log("data",o)})).catch((o=>{console.log("error",o)}));
-//# sourceMappingURL=catFind.8855435b.js.map
